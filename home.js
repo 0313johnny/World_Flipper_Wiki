f5e03c0180a4d138c142788788eeecbb37e09403
Meteoroid_Kun <31439634+0313johnny@users.noreply.github.com>
2021-01-05 11:42:35 +0800

    0105

--- home.js
+++ home.js
@@ -1,7 +1,9 @@
 //World Flipper用的表 https://docs.google.com/spreadsheets/d/1YihcIS-7iApI1GI4pN6XYwyfndb5CbNwAVUEvEl9jyA/edit#gid=0
+
 function start(){
-    document.addEventListener("mousedown", show_imformation, false);
+    document.addEventListener("mousedown", show_info, false);
 }
+
 function createNewNode(text ){
     var newE= document.createElement("img");
     var currentNode = document.getElementById( "item_list" );
@@ -11,22 +13,22 @@ function createNewNode(text ){
 }
 
 // 滑鼠點下會跳到這個function，用途為判斷滑鼠在哪
-function show_imformation(e){
+function show_info(e){
 
     // 若是沒有點到圖片，則隱藏介紹框。
-    document.getElementById("imfor_block_set_id").style.display="none";
+    document.getElementById("info_block_set_id").style.display="none";
 
     if ( e.target.id.toLowerCase().substr(0, 5) == "item_"){
         // 確認按下的鍵是滑鼠左鍵（button==1）
         if(e.buttons== 1){
-            var clicked_block= document.getElementById(e.target.id.toLowerCase())
-            for(var i=0; i<DataBase.length; i++){
-                if(DataBase[i][1].v == clicked_block.id.substr(5))
+            var clicked_block = document.getElementById(e.target.id.toLowerCase())
+            for( var i = 0 ; i < DataBase.length ; i++ ) {
+                if( DataBase[i][1].v == clicked_block.id.substr(5) )
                 {
                     // 顯示介紹框，以及更改內容
-                    document.getElementById("imfor_block_set_id").style.display="block";
-                    document.getElementById("imfor_name").innerHTML= "名稱："+DataBase[i][3].v;
-                    document.getElementById("imfor_contribute").innerHTML= "介紹："+DataBase[i][4].v;
+                    document.getElementById("info_block_set_id").style.display="block";
+                    document.getElementById("info_name").innerHTML= "名稱：" + DataBase[i][3].v;
+                    document.getElementById("info_contribute").innerHTML= "介紹：" + DataBase[i][4].v;
                     break;
                 }
                 
@@ -38,6 +40,3 @@ function show_imformation(e){
 
 
 window.addEventListener("load", start, false);
-
-
-
